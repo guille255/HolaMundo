ae3095df005da9792181ad8f5e4b0c8e57cb728a
guille255 <guille255@gmail.com>
2025-08-29 18:35:13 +0200

    Se agregó una línea de código inservible

--- src/com/acme/PersonaRegistro.java
+++ src/com/acme/PersonaRegistro.java
@@ -4,6 +4,7 @@ public record PersonaRegistro(String nombre, String apellido) {
 
     @Override
     public String toString() {
+        System.out.println("Esta instucción es inservible");
         return "%s %s".formatted(nombre, apellido);
     }
 }

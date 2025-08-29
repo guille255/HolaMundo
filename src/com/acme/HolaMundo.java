ab738828bffe1cf25547a6e75198286b9a35098b
guille255 <guille255@gmail.com>
2025-08-29 18:17:30 +0200

    Se creó la clase persona

--- src/com/acme/HolaMundo.java
+++ src/com/acme/HolaMundo.java
@@ -5,7 +5,9 @@ public class HolaMundo {
 
         Mensaje msj = new Mensaje();
         String nombre = msj.solicitaDatos("Escribe tu nombre: ");
-        msj.saludar(nombre);
+        String apellidos = msj.solicitaDatos("Escribe tu apellido: ");
+        Persona p1 = new Persona(nombre, apellidos);
+        msj.saludar(p1);
 
     }
 

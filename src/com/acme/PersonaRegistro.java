113191fa30bc258e3948c1669da45a8e25a7b2c8
Guillermo Garcia <guille255@gmail.com>
2025-08-29 19:20:04 +0200

    Comentario sobre codigo inservible.

--- src/com/acme/PersonaRegistro.java
+++ src/com/acme/PersonaRegistro.java
@@ -4,7 +4,8 @@ public record PersonaRegistro(String nombre, String apellido) {
 
     @Override
     public String toString() {
-        System.out.println("Esta instucción es inservible");
+        // Guillermo comenté la línea que considero se puede eliminar.
+        // System.out.println("Esta instucción es inservible");
         return "%s %s".formatted(nombre, apellido);
     }
 }
